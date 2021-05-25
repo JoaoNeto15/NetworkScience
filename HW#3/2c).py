c9d55ef50972d5d68c8cdf140e932c556048c31a
JoaoNeto15 <44527194+JoaoNeto15@users.noreply.github.com>
2021-05-25 20:29:39 +0100

    2c) almost done

--- HW#3/2c).py
+++ HW#3/2c).py
@@ -14,16 +14,24 @@ countU=0
 list=[]
 
 
+
 for i in G.nodes():
     
     if (i%10==0) or  (i%10==1)or (i%10==2)or (i%10==3):
-        G.add_node(i,vote="A",degree=G.neighbors(i)) # procurar como se faz size
-        list.append((i,)) # terminar
+        G.add_node(i,vote="A",degree=G.degree(i)) # procurar como se faz size
+        list.append((i,G.degree(i))) # terminar
         countA+=1
     elif (i%10==4) or  (i%10==5) or (i%10==6) or (i%10==7):
         G.add_node(i,vote="B",degree=G.neighbors(i))
+        list.append((i,G.degree(i)))
         countB+=1
     else:
         G.add_node(i,vote="U",degree=G.neighbors(i))
+        list.append((i,G.degree(i)))
         countU+=1
 
+# ordenação
+list.sort(reverse= True,key=lambda a: a[1])
+
+print(list[:20])
+print(list)
