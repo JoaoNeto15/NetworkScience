1562aefc8c48963d2e5cec24fe751298a677a1de
diana-egas <38374862+diana-egas@users.noreply.github.com>
2021-07-18 15:09:29 +0100

    Add files via upload
    Final

--- projeto final/mapa_rangel_paises.py
+++ projeto final/mapa_rangel_paises.py
@@ -3,23 +3,20 @@
 import csv
 import pandas as pd
 
-ficheiro = open('NosEmFalta.csv', 'r')
+ficheiro = open('Porto_Lisboa.csv', 'r')
 reader = csv.reader(ficheiro)
-s=""
 
-with open("info_paisesBK.csv", 'w',newline='',encoding = 'utf8') as csvfile:
+with open("info_paises.csv", 'w',newline='') as csvfile:
     #legenda = ["IATA", "LAT", "LONG"]
-    csvwriter = csv.writer(csvfile)
+    csvwriter = csv.writer(csvfile, delimiter=',')
     #csvwriter.writerow(legenda)
     for linha in reader:
         #print linha[1]
-        s = linha[0]
-        print(s)
+        s = linha[1]
 
-        with open("fligths_nodes.csv",'r', errors='ignore') as f_obj:
-            reader2 = csv.reader(f_obj)
-            
-            for line in reader2:      #Iterates through the rows of your csv
+        with open("fligths_nodes.csv") as f_obj:
+            reader = csv.reader(f_obj, delimiter=',')
+            for line in reader:      #Iterates through the rows of your csv
                 # line 6 : iata
                 # line 8: latitude
                 # line 9: longitude
@@ -27,17 +24,19 @@ with open("info_paisesBK.csv", 'w',newline='',encoding = 'utf8') as csvfile:
                 if line[6] == s:
                     lista = []
                     # lista.append(s)
-                    # lista.append(float(line[8]))
-                    # lista.append(float(line[9]))
-                    #lista.append(line[5])
-                    lista.append(line)
+
+                    lista.append(line[3])
+                    lista.append(line[4])
+                    lista.append(line[5])
+                    lista.append(line[6])
+                    lista.append(float(line[8]))
+                    lista.append(float(line[9]))
+                    lista.append(line[10])
+                    lista.append(line[11])
+                    lista.append(line[12])
+                    lista.append(line[13])
                     #for i in range(0,len(lista),4): # step by threes.
-                    print(line)
-                    l=""
-                    for i in line:
-                        #print("i: "+i)
-                        l=l+i
-                    #print(l)
-                    csvwriter.writerow(line) 
+                    csvwriter.writerow(lista)
                     break
-                
+                    #for i in range(0,len(lista),4): # step by threes.
+                        #csvwriter.writerow(lista[i:i+4])
